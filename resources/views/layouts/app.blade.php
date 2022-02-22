c0296a1e7f1bfc754f235f461a7eba73247f23c2
Juan Pablo Grijalba <grjuan013@gmail.com>
2022-02-22 09:20:03 -0500

    Commit inicial

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-light" style="background-color: #029643;">
+            <div class="container">
+                <ul class="nav" >
+                    <li class="nav-item">
+                        <a class="nav-link" style="color:black;" href="{{ url('/') }}"><i class="bi bi-house-fill"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color:black;"><i class="bi bi-search"></i></a>
+                    </li>
+                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                        <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+                    </form>
+                </ul>
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav">
+                        
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav justify-content-end">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color:black;" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" style="color:black;" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" style="color:black;" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
---
+++ resources/views/mainPage.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    
+</head>
+<body>
+    <header>
+        <div class="container">    
+          <nav class="izq">
+            <ul>
+              <li><a href="#"><img src="/images/home.png" style="height: 20px;"></a></li>
+              <li><a href="#"><img src="/images/search.png" style="height: 20px;"></a></a></li>
+              <li> <input type="search" class="busqueda" placeholder=""></li>
+            </ul>
+          </nav>
+          <nav class="der">
+            <ul>
+              <li><a href="#"><img src="/images/user.png" style="height: 20px;"></a></li>
+              <li><a href="#"><img src="/images/menu.png" style="height: 20px;"></a></li>
+            </ul>
+          </nav>
+        </div>
+    </header>
+    <div class="imagenes">
+        <img src="/images/logoudenar.jpg" class="logoudenar"><br>
+        <img src="/images/huella.png" class="logohuella">
+    </div>
+    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam euismod magna, eu dignissim mi pulvinar et.</h3><br>
+    <div>
+        <div class="info">
+            <img src="/images/question.png" class="pregunta">
+            <button class="btn_quest">¿Qué es la Huella de Carbono?</button>
+        </div>
+         <div class="info">
+            <img src="/images/leaf.png" class="hoja">
+            <button class="btn_calc">Calcula tu Huella de Carbono</button>
+        </div>
+         <div class="info">
+            <img src="/images/information.png" class="informacion">
+            <button class="btn_info">Podría interesarte...</button>
+        </div>
+        
+    </div>
+    
+</body>
+</html>
